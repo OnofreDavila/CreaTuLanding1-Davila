0f4f93c28a4a0afd2df5585830430a27039a6b58
onofre davila <onofrej14@gmail.com>
2026-02-14 16:39:03 -0300

    añadiendo sweetAlert2

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const ItemCount = ({ stock, onAdd }) => {
   //Estado del contador
   const [counter, setCounter] = useState(1);
+  const navigate = useNavigate();
 
   //funciones de los botones
   const sumar = () => {
@@ -22,21 +24,32 @@ export const ItemCount = ({ stock, onAdd }) => {
   };
 
   return (
-    <div>
-      <button className="btn btn-danger" onClick={restar}>
-        -
-      </button>
-      <span className="btn">{counter}</span>
-      <button className="btn btn-success" onClick={sumar}>
-        +
-      </button>
-      <button
-        className="btn btn-primary m-2"
-        onClick={comprar}
-        disabled={counter === 0 || stock === 0}
-      >
-        Comprar
-      </button>
-    </div>
+    <>
+      {stock > 0 ? (
+        <div>
+          <button className="btn btn-danger" onClick={restar}>
+            -
+          </button>
+          <span className="btn">{counter}</span>
+          <button className="btn btn-success" onClick={sumar}>
+            +
+          </button>
+          <button
+            className="btn btn-primary m-2"
+            onClick={comprar}
+            disabled={counter === 0 || stock === 0}
+          >
+            Comprar
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p>Lo sentimos, por el momento no hay stock disponible 😭</p>
+          <button className="btn btn-dark" onClick={() => navigate(-1)}>
+            Volver atrás
+          </button>
+        </div>
+      )}
+    </>
   );
 };
